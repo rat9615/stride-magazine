909c68858178815f469ecbb28c183793f1106636
rohanr <rohanrocket4@gmail.com>
2020-11-17 00:16:02 +0530

    fix: Refactor codebase

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const app = express();
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const path = require('path');
+const favicon = require('serve-favicon');
 const server = require('http').Server(app);
 
 const { ATLAS_URI } = process.env;
 
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 
 mongoose.connect(ATLAS_URI, {
   useNewUrlParser: true,
