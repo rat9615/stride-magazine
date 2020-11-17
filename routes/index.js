704824f6ac3b288ea99b99b85ed1f80afcef2bf2
rohanr <rohanrocket4@gmail.com>
2020-11-17 16:40:58 +0530

    feat: Access magazine by button click

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const grid = require('gridfs-stream');
+
+const db = mongoose.connection;
+
+let Grid;
+db.on('error', console.error.bind(console, 'Connection Error'));
+db.once('open', () => {
+  console.log('GridFS Connected Successfully');
+  Grid = grid(db.db, mongoose.mongo);
+});
+
 router.get('/', (req, res) => res.render('index'));
+
+router.get('/pdf/:id', (req, res) => {
+  const readstream = Grid.createReadStream({ _id: req.params.id });
+  res.setHeader('Content-disposition', `filename= Stride2020.pdf`);
+  res.setHeader('Content-type', 'application/pdf');
+  readstream.pipe(res);
+});
 
 module.exports = router;
