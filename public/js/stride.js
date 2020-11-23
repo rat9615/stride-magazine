bbc3dd263df04dcdd9368eb6ae272f524cf05220
rohanr <rohanrocket4@gmail.com>
2020-11-23 20:42:24 +0530

    feat: Add view counter

--- public/js/stride.js
+++ public/js/stride.js
@@ -5,3 +5,12 @@ $(document).ready(function () {
     placement: 'auto',
   });
 });
+
+// $('#views').ready(function (e) {
+//   const textLength = $('#views').text();
+//   if (textLength >= 100) {
+//     $('#views').css('font-size', '2.5rem');
+//   } else if (textLength >= 1000) {
+//     $('#views').css('font-size', '2rem');
+//   }
+// });
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,37 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 const grid = require('gridfs-stream');
+const storage = require('node-persist');
 
 const db = mongoose.connection;
-
 let Grid;
+let views = 0;
+
 db.on('error', console.error.bind(console, 'Connection Error'));
 db.once('open', () => {
   console.log('GridFS Connected Successfully');
   Grid = grid(db.db, mongoose.mongo);
 });
 
-router.get('/', (req, res) => res.render('index'));
+storage
+  .init()
+  .then(() => storage.getItem('views'))
+  .then((value) => {
+    if (value > 0) {
+      views = value;
+    } else {
+      views = 0;
+    }
+  });
+
+router.get('/', async (req, res) => {
+  // eslint-disable-next-line no-plusplus
+  views++;
+  storage.setItem('views', views).then(async () => {
+    await res.json(views);
+  });
+  await res.render('index', { views });
+});
 
 router.get('/pdf/:id', (req, res) => {
   const readstream = Grid.createReadStream({ _id: req.params.id });
